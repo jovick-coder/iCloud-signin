d375c74394eebe3f05c6ea068102c06ee1d5dca4
Jovick-coder <victorjosiahm3@gmail.com>
2021-05-02 22:58:02 +0100

    main js"

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function submit() {
 	var id = document.getElementById('id').value;
 	var pass = document.getElementById('pass').value;
 	var message = document.getElementById('message');
-	if (pass === pass || id === id) {
-		message.innerHTML = 'incorrect';
+	if (pass == pass || id == id) {
+		message.innerHTML = 'Access Denied';
 	}
 }
