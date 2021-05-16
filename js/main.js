8b87ab51c8527607fc876694a799fa2027667513
Jovick-coder <victorjosiahm3@gmail.com>
2021-05-16 17:19:23 +0100

    added friends to the chat page arrange the icons for mobile view

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@ function submit() {
 	var id = document.getElementById('id').value;
 	var pass = document.getElementById('pass').value;
 	var message = document.getElementById('message');
+	var smessage = document.getElementById('smessage');
 	if (pass != 1234 || id != 1234) {
 		message.innerHTML = 'Access Denied';
+		setTimeout(function () {
+			message.innerHTML = '';
+
+		}, 1000);
 	} else {
-		message.innerHTML = 'Access Granted';
+		smessage.innerHTML = 'Access Granted';
 		setTimeout(function () {
 			window.location.href = '/ChatPage.html';
-		}, 5000);
+		}, 1000);
 	}
 }
