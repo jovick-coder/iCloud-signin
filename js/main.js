404104a5c1cf9223e7ec4456a2c991dd86bbb899
Jovick-coder <victorjosiahm3@gmail.com>
2021-05-16 16:53:45 +0100

    worked on the loging button to redirect to the chat page after 5sec

--- js/main.js
+++ js/main.js
@@ -4,5 +4,10 @@ function submit() {
 	var message = document.getElementById('message');
 	if (pass != 1234 || id != 1234) {
 		message.innerHTML = 'Access Denied';
+	} else {
+		message.innerHTML = 'Access Granted';
+		setTimeout(function () {
+			window.location.href = '/ChatPage.html';
+		}, 5000);
 	}
 }
